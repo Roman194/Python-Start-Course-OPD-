ca96af010ae648b93df6d5317289b783bebc35b0
Roman194 <RAZ3113@yandex.ru>
2023-02-24 12:57:31 +0600

    Final version of course

--- Эпизод 1. Введение/Задание 1.3/task.py
+++ Эпизод 1. Введение/Задание 1.3/task.py
@@ -34,23 +34,21 @@ def task_3(n):
 # Пример 4
 def task_4(x, n):
     # TODO
-    i=2
-    z=0
-    while i<=n:
-        z*=(x+i)/i
-        i+=1
-    return z
+    x=1
+    y=1
+    for i in range(2,n+1):
+        x*=(y+i)/i
+    return x
 
 
 # Пример 5
 def task_5(x, n):
     # TODO
+    x=0
     y=1
-    i=1
-    while i<=n:
-        y+=(x**i)/(i*2)
-        i+=2
-    return y
+    for i in range(1,n+1):
+        x+=(y*i)/(i*2)
+    return x
 
 
 # Пример 6
